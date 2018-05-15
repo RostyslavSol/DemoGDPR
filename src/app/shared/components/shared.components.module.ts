c17ec540ad616a4252aaf99447c80bd1eb183eee
rost <rsolopatych@gmail.com>
2018-05-15 17:46:31 +0200

    Remove loader

--- src/app/shared/components/shared.components.module.ts
+++ src/app/shared/components/shared.components.module.ts
@@ -2,12 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { LoaderComponent } from './loader/loader.component';
 
 
 const components = [
   HeaderComponent,
-  LoaderComponent
 ];
 
 @NgModule({
